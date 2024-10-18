9cc8ca116c4b4c9b6c7be4579b7080b304ff8896
Rohansiddu71 <rohansiddu71@gmail.com>
2024-10-19 01:21:38 +0530

    Remove feature.py file

--- feature.py
+++
@@ -1,7 +0,0 @@
-# Program to print first name and last name
-
-# Get user input for first name and last name
-first_name = "Rohan"
-last_name = "Siddhu k s"
-# Print the full name
-print("Your name is:", first_name, last_name)
