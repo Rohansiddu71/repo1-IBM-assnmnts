3ad1e5d0f26f4bfefac8cc26d9be00b69a298d5e
Rohan Siddhu K S <rohansiddu71@gmail.com>
2024-10-24 22:46:16 +0530

    Initial commit

---
+++ feature.py
@@ -0,0 +1,8 @@
+# Program to print first name and last name
+
+# Get user input for first name and last name
+first_name ="Rohan"
+last_name ="Siddhu K S "
+
+# Print the full name
+print("Your name is:", first_name, last_name)
